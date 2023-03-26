0a0520f9785836973a4eb7c00e672d83edbc4919
Mauri Sandberg <maukka@ext.kapsi.fi>
2023-03-26 20:10:01 +0300

    orion5x: add base-files

---
+++ target/linux/orion5x/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,15 @@
+REQUIRE_IMAGE_METADATA=1
+
+platform_check_image() {
+	return 0
+}
+
+platform_do_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	*)
+		default_do_upgrade "$1"
+		;;
+	esac
+}
